944834e83a20deb120abfe888df3413187eedb32
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-18 23:57:14 +0300

    Route params

--- src/index.js
+++ src/index.js
@@ -4,18 +4,30 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+    {id:1,username:"yuran",displayname:"Yuran"},
+    {id:2,username:"Oscar",displayname:"Oscar"},
+    {id:3,username:"Nhassengo",displayname:"Nhassengo"}
+
+];
+
 app.get("/", (req,res) =>{
     res.status(201).send({msg:"Hello"});
 });
 
 app.get('/api/users',(req,res) =>{
-    res.send([
-        {id:1,username:"yuran",displayname:"Yuran"},
-        {id:2,username:"Oscar",displayname:"Oscar"},
-        {id:3,username:"Nhassengo",displayname:"Nhassengo"}
-
-    ]);
+    res.send(mockUsers);
 });
+
+app.get('/api/users/:id', (req,res) =>{
+    console.log(req.params);
+    const parseId = parseInt(req.params.id);
+    if(isNaN(parseId)) res.status(400).send({msg:'Bad Request. Invalid ID.'});
+
+    const findUser = mockUsers.find((user) => user.id === parseId);
+    if(!findUser) return res.sendStatus(404);
+    return res.send(findUser);
+})
 
 app.get('/api/product', (req,res) =>{
     res.send([
