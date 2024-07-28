83ec78c1c753b4322c81d4cd375b6642c73169d3
yurannhassengo <yurannhassengo@gmail.com>
2024-07-28 14:13:32 -0700

    Add delete method

--- src/index.js
+++ src/index.js
@@ -88,7 +88,22 @@ app.patch('/api/users/:id',(req,res) =>{
 
 });
 
+app.delete('/api/users/:id',(req,res) =>{
+    const {params:{id}} =req;
 
+    const parsedId = parseInt(id);
+
+    if(isNaN(parsedId)) return res.sendStatus(400);
+
+    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+
+    if(findUserIndex === -1) return res.sendStatus(404);
+
+
+    mockUsers.splice(parsedId,1);
+
+    return res.sendStatus(200);
+})
 
 
 app.listen(PORT,() =>{
