74e581b3e199c5aef4a68fabc74a4ed115842e37
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-28 23:10:55 +0300

    Add patch method

--- src/index.js
+++ src/index.js
@@ -68,7 +68,28 @@ app.put('/api/users/:id',(req,res) =>{
 
     mockUsers[findUserIndex] = {id:parsedId,...body};
     return res.sendStatus(200);
-})
+});
+
+app.patch('/api/users/:id',(req,res) =>{
+    const {
+        body,
+        params:{id},
+    } = req;
+
+    const parsedId = parseInt(id);
+
+    if(isNaN(parsedId)) return res.sendStatus(400);
+    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+
+    if(findUserIndex === -1) return res.sendStatus(404);
+
+    mockUsers[findUserIndex] ={...mockUsers[findUserIndex],...body};
+    return res.sendStatus(200);
+
+});
+
+
+
 
 app.listen(PORT,() =>{
     console.log(`Running on Port ${PORT}`)
